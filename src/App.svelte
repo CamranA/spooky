1cbd4bda211d7a8744a35b181ca68d127622921b
Cam A <camran.ahmad@gmail.com>
2022-09-25 17:52:08 -0700

    not sure

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,12 @@
   import nine11 from './lib/routes/nine11.svelte'
   import QShaman from './lib/routes/QSham.svelte'
 </script>
-
+<ul>
+  <li style="display:inline;"><a href="/">Home</a></li>
+  <li style="display:inline;"><a href="./#/Birds">Birds</a></li>
+  <li style="display:inline;"><a href="./#/nine11">9/11</a></li>
+  <li style="display:inline;"><a href="./#/QShaman">QShaman</a></li>
+</ul>
 <Router routes={{
   '/': Home,
   '/Birds': Birds,
