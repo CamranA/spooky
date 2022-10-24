2697d4fd4d9567b2baa691e95fa6a3cfe68d751b
Cam A <camran.ahmad@gmail.com>
2022-10-24 08:49:19 -0700

    ok

--- src/lib/routes/QSham.svelte
+++ src/lib/routes/QSham.svelte
@@ -5,18 +5,27 @@
 
 <Main>
     <span slot="theTitle">Q Shaman Intro</span>
-    <span slot="theBody">Q Shaman is here</span>
+    <span slot="theBody"><p>I am here to reclaim the term "conspiracy theory". The term conspiracy theory has been
+        weaponized against individuals searching for the truth, individuals like you and me. It has limited the range of
+        subjects that we discuss, and has allowed powerful actors to escape accountability. They are dismissing
+        legitimate concerns as conspiracy theories and cutting off the examination of corruption. Examing corruption is
+        something I have spent decades doing dating back to studies on the Soviet Union, and it is something you are
+        going to begin doing tonight.
+        It is not an outlandish thing to say a conspiracy theory exists and they are fairly common. Follow along the
+        clues I have laid out for you, and maybe... just maybe... you will uncover the truth.</p></span>
     <span slot="theByline">
-        <Byline imgSource={'https://ichef.bbci.co.uk/images/ic/1920x1080/p098vq4z.jpg'}> 
-            <span slot="bylineText">This is
-            the QShamans profileThis is the QShamans profileThis is the QShamans profileThis is the QShamans profileThis
-            is the QShamans profileThis is the QShamans profileThis is the QShamans profileThis is the QShamans
-            profileThis is the QShamans profileThis is the QShamans profileThis is the QShamans profileThis is the
-            QShamans profileThis is the QShamans profileThis is the QShamans profileThis is the QShamans profileThis is
-            the QShamans profileThis is the QShamans profileThis is the QShamans profileThis is the QShamans profileThis
-            is the QShamans profileThis is the QShamans profileThis is the QShamans profileThis is the QShamans
-            profileThis is the QShamans profile
-            </span>
-        </Byline>
+        <Byline imgSource={'https://ichef.bbci.co.uk/images/ic/1920x1080/p098vq4z.jpg'}> <span slot="bylineText">My actions were not an attack on this country. That is incorrect. That is inaccurate, entirely.
+            Well, I sang a song. And that's a part of shamanism. It's about-- creating positive vibrations in a sacred chamber.
+            I also stopped people from stealing and vandalizing that sacred space, the Senate. Okay?
+            I actually stopped somebody from stealing muffins out of the-- out of the break room.
+            And I also said a prayer in that sacred chamber.
+            Because it was my intention to bring divinity, and to bring God back into the Senate.
+            And that is-- and that is the one very serious regret that I have, was believing that when we were waved in by police officers, that it was acceptable.
+            I consider myself a lover of my country.
+            I consider myself a believer in the Constitution.
+            I consider myself a believer in truth and our founding principles.
+            I consider myself a believer in God.
+    </span>
+    </Byline>
     </span>
 </Main>
